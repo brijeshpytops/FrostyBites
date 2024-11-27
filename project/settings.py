424b28592b2dafeae9d53288a0ee012786a15b40
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-11-27 15:21:21 +0530

    finally project is done

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b8_2g#8*zyne(t1s)ebjpml&%u7czc01i0ivdg-sl9!aiev_#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
